99adeb10d7a639537b329f6e18fb7fdf4c766f0c
dadepu <danielkoellgen@gmail.com>
2022-04-22 17:43:03 +0200

    initial

---
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/SrscsProdTestServiceApplication.java
@@ -0,0 +1,13 @@
+package de.danielkoellgen.srscsprodtestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrscsProdTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrscsProdTestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/danielkoellgen/srscsprodtestservice/SrscsProdTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danielkoellgen.srscsprodtestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrscsProdTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
