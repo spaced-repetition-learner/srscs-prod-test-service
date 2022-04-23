a7186623b129478194a613ca5a001f14bc30a6dd
dadepu <danielkoellgen@gmail.com>
2022-04-23 10:29:35 +0200

    feat(card): add Card

---
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/domain/card/domain/Card.java
@@ -0,0 +1,37 @@
+package de.danielkoellgen.srscsprodtestservice.domain.card.domain;
+
+import de.danielkoellgen.srscsprodtestservice.domain.deck.domain.Deck;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "cards")
+@NoArgsConstructor
+public class Card {
+
+    @Id
+    @Getter
+    @Column(name = "card_id")
+    @Type(type = "uuid-char")
+    private @NotNull UUID cardId;
+
+    @Getter
+    @ManyToOne
+    @JoinColumn(name = "deck_id")
+    private @NotNull Deck deck;
+
+    @Getter
+    @Column(name = "is_active")
+    private @NotNull Boolean isActive;
+
+    public Card(@NotNull UUID cardId, @NotNull Deck deck, @NotNull Boolean isActive) {
+        this.cardId = cardId;
+        this.deck = deck;
+        this.isActive = isActive;
+    }
+}
