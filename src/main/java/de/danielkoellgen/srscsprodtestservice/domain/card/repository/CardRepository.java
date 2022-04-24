45bb4bf6fda07b7699450987731187bfd0fd2181
dadepu <danielkoellgen@gmail.com>
2022-04-24 10:28:23 +0200

    feat(card): allow to find Cards by isActive

--- src/main/java/de/danielkoellgen/srscsprodtestservice/domain/card/repository/CardRepository.java
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/domain/card/repository/CardRepository.java
@@ -1,9 +1,12 @@
 package de.danielkoellgen.srscsprodtestservice.domain.card.repository;
 
 import de.danielkoellgen.srscsprodtestservice.domain.card.domain.Card;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public interface CardRepository extends CrudRepository<Card, UUID> {
+
+    @NotNull Iterable<Card> findByIsActive(@NotNull Boolean isActive);
 }
