df473a5cf50ee0e3a1217179fc2966b2d3eb13ab
dadepu <danielkoellgen@gmail.com>
2022-04-25 19:10:50 +0200

    feat: add ConsumerEvent

---
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/events/consumer/AbstractConsumerEvent.java
@@ -0,0 +1,82 @@
+package de.danielkoellgen.srscsprodtestservice.events.consumer;
+
+import de.danielkoellgen.srscsprodtestservice.domain.domainprimitive.EventDateTime;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+abstract public class AbstractConsumerEvent implements ConsumerEvent {
+
+    protected final @NotNull UUID eventId;
+
+    protected final @NotNull UUID transactionId;
+
+    protected       @Nullable UUID correlationId;
+
+    protected final @NotNull String eventName;
+
+    protected final @NotNull EventDateTime occurredAt;
+
+    protected final @NotNull EventDateTime receivedAt;
+
+    protected final @NotNull String topic;
+
+    public AbstractConsumerEvent(@NotNull ConsumerRecord<String, String> event) {
+        this.eventId = UUID.fromString(getHeaderValue(event, "eventId"));
+        this.transactionId = UUID.fromString(getHeaderValue(event, "transactionId"));
+        try {
+            this.correlationId = UUID.fromString(getHeaderValue(event, "correlationId"));
+        } catch (Exception e) {
+            this.correlationId = null;
+        }
+        this.eventName = getHeaderValue(event, "type");
+        this.occurredAt = EventDateTime.makeFromFormattedString(getHeaderValue(event, "timestamp"));
+        this.receivedAt = new EventDateTime(LocalDateTime.now());
+        this.topic = event.topic();
+    }
+
+    abstract public void execute();
+
+    public static String getHeaderValue(ConsumerRecord<String, String> event, String key) {
+        return new String(event.headers().lastHeader(key).value(), StandardCharsets.US_ASCII);
+    }
+
+    @Override
+    public @NotNull UUID getEventId() {
+        return eventId;
+    }
+
+    @Override
+    public @NotNull UUID getTransactionId() {
+        return transactionId;
+    }
+
+    @Override
+    public @Nullable UUID getCorrelationId() {
+        return correlationId;
+    }
+
+    @Override
+    public @NotNull String getEventName() {
+        return eventName;
+    }
+
+    @Override
+    public @NotNull EventDateTime getOccurredAt() {
+        return occurredAt;
+    }
+
+    @Override
+    public @NotNull EventDateTime getReceivedAt() {
+        return receivedAt;
+    }
+
+    @Override
+    public @NotNull String getTopic() {
+        return topic;
+    }
+}
---
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/events/consumer/ConsumerEvent.java
@@ -0,0 +1,26 @@
+package de.danielkoellgen.srscsprodtestservice.events.consumer;
+
+import de.danielkoellgen.srscsprodtestservice.domain.domainprimitive.EventDateTime;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public interface ConsumerEvent {
+
+    @NotNull UUID getEventId();
+
+    @NotNull UUID getTransactionId();
+
+    @Nullable UUID getCorrelationId();
+
+    @NotNull String getEventName();
+
+    @NotNull EventDateTime getOccurredAt();
+
+    @NotNull EventDateTime getReceivedAt();
+
+    @NotNull String getTopic();
+
+    @NotNull String getSerializedContent();
+}
