7286c1a35717d8c25a55244f9dd20159806a0460
dadepu <84119713+dadepu@users.noreply.github.com>
2022-08-01 13:53:18 +0200

    feat(collaboration): allow to find all Collaborations

--- src/main/java/de/danielkoellgen/srscsprodtestservice/domain/collaboration/repository/CollaborationRepository.java
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/domain/collaboration/repository/CollaborationRepository.java
@@ -1,15 +1,17 @@
 package de.danielkoellgen.srscsprodtestservice.domain.collaboration.repository;
 
 import de.danielkoellgen.srscsprodtestservice.domain.collaboration.domain.Collaboration;
-import de.danielkoellgen.srscsprodtestservice.domain.participant.domain.Participant;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CollaborationRepository extends CrudRepository<Collaboration, UUID> {
 
     @Nullable Collaboration findByParticipants_ParticipantId(@NotNull UUID participantId);
+
+    @NotNull List<Collaboration> findAll();
 
 }
