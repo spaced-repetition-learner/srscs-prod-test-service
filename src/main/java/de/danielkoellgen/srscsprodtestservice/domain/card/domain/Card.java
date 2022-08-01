be5fa406ce327175d779fd8e018b83cc6435d495
dadepu <84119713+dadepu@users.noreply.github.com>
2022-08-01 13:51:11 +0200

    feat(card): allow to make Card from DTO

--- src/main/java/de/danielkoellgen/srscsprodtestservice/domain/card/domain/Card.java
+++ src/main/java/de/danielkoellgen/srscsprodtestservice/domain/card/domain/Card.java
@@ -1,6 +1,7 @@
 package de.danielkoellgen.srscsprodtestservice.domain.card.domain;
 
 import de.danielkoellgen.srscsprodtestservice.domain.deck.domain.Deck;
+import de.danielkoellgen.srscsprodtestservice.web.deckservice.dto.CardResponseDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
@@ -21,7 +22,7 @@ public class Card {
     private @NotNull UUID cardId;
 
     @Getter
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "deck_id")
     private @NotNull Deck deck;
 
@@ -33,6 +34,10 @@ public class Card {
         this.cardId = cardId;
         this.deck = deck;
         this.isActive = isActive;
+    }
+
+    public static @NotNull Card makeFromDto(@NotNull CardResponseDto dto, @NotNull Deck deck) {
+        return new Card(dto.cardId(), deck, dto.getIsActive());
     }
 
     public void disableCard() {
